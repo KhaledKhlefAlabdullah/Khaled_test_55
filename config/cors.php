c6f3994e238423a5e24110fb063525b737e4fce5
MUHMAD OMAR <m7mmad.omar0@gmail.com>
2024-02-16 21:02:27 +0300

    Fix CORS for front

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,12 @@ return [
     |
     */
 
-    'paths' => ['*'],
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:4200')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://160.16.143.161')],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +30,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];

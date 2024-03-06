66e1ae019f12b4656249d45f5247e70d39dca4f3
khaled <khaledabdullah2001104@gmail.com>
2024-03-06 18:57:18 +0300

    finshed events and add inquiry

---
+++ app/Models/EventResources.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\HasUuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class EventResources extends Model
+{
+    use HasFactory, HasUuid, SoftDeletes;
+
+    protected $keyType = 'string';
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+
+    protected $fillable=[
+        'event_id',
+        'resource_id'
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,14 @@ use App\Http\Controllers\Notification\NotificationsSettingController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegistrationRequestController;
+use App\Http\Controllers\ResourceController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\ShippingController;
 use App\Http\Controllers\StakeholderController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\Timelines\TimelineController;
 use App\Http\Controllers\Timelines\TimelineEventController;
+use App\Http\Controllers\Timelines\TimelineQuiresController;
 use App\Http\Controllers\Timelines\TimelineSharesRequestController;
 use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\User\UserProfileController;
@@ -198,10 +200,29 @@ Route::group(['prefix' => 'api'], function () {
 
                     Route::get('/{id}',[TimelineEventController::class,'show']);
 
+                    Route::post('/add',[TimelineEventController::class,'store']);
+                    
+                    Route::put('/edit/{id}',[TimelineEventController::class,'update']);
+
+                    Route::delete('/delete/{id}',[TimelineEventController::class,'destroy']);
+
+                    // For inquiries
+                    Route::group(['prefix' => 'inquiries'],function(){
+
+                        Route::post('/add',[TimelineQuiresController::class,'store']);
+
+                    });
+
                 });
 
             });
 
+            // For Resouces
+            Route::group(['prefix' => 'resources'],function(){
+
+                Route::get('/',[ResourceController::class,'index']);
+
+            });
 
             Route::group(['prefix' => 'status-report', 'controller' => \App\Http\Controllers\StatusReportController::class], function () {
                 // Generate & Preview Status report

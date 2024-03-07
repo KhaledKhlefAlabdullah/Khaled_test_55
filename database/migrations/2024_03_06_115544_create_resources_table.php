4738d791bc45764caaf4734c0b79659fe9955baf
khaled <khaledabdullah2001104@gmail.com>
2024-03-07 14:19:53 +0300

    fixe conflect

--- database/migrations/2024_03_06_115544_create_resources_table.php
+++ database/migrations/2024_03_06_115544_create_resources_table.php
@@ -16,11 +16,7 @@ return new class extends Migration
             $table->string('stakeholder_id');
             $table->string('resource');
             $table->float('quantity');
-<<<<<<< HEAD
-            $table->boolean('is_avilable')->default(true);
-=======
             $table->boolean('is_available')->default(true); // Set default value to boolean true
->>>>>>> 1c81ab805ad350e6698834f2dc49e83a887e715f
             $table->foreign('stakeholder_id')->references('id')->on('stakeholders')->onDelete('cascade');
             $table->timestamps();
             $table->softDeletes();
